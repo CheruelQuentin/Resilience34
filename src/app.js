ccd8e85be015819460fecdb8040d1f98ff55681f
CheruelQuentin <65130218+CheruelQuentin@users.noreply.github.com>
2022-03-21 19:49:28 +0100

    fix 2FA

--- src/app.js
+++ src/app.js
@@ -12,11 +12,12 @@ const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname,'../templates/partials')
 
 const connectUser = require('./utils/connectUser.js')
-const authenticateDN = require('./utils/ldapConnect.js')
+
 const ipIssue = require('./utils/nodemailer.js')
 const validateConnecting  = require('./utils/2FA.js')
 const res = require('express/lib/response')
 const messagebird = require('messagebird')('Hyn3Cut3NcJK0UXnB2K6EYZG0');
+var ldap = require('ldapjs')
 
 // Setup handlebars engine and views location 
 app.set('view engine','hbs')
@@ -29,75 +30,49 @@ app.use(bodyParser.urlencoded({ extended: true }))
 const db = mysql.createConnection({ 
   host :'localhost',
   user : 'root',
-  password : '',
+  password : 'password',
   database : 'resilience34'
   
 })
 
-
-
-
-function authenticateDN(username, password){
-  var client = ldap.createClient({
-    url: 'ldap://192.168.1.18:5389'
-  })
-  client.bind(username,password,function(err){
-    if(err){
-      console.log("Error in new connection "+ err.code +err);
-    } else {
-      console.log("success")
-	//  addUser();
-		var authenticateldap = true
-		connexion(authenticateldap)
-    }
-  })
-}
-
-
-function connexion(authenticateldap) {
+function connexion(authenticateldap, username,res) {
   if(authenticateldap == true) {
     var params = {
       originator: '',
       type: 'sms'
     }
     messagebird.verify.create('+33651266267', params, function (error, response) {
-                    if (error) {
-                      //request has failed
-                      console.log(error);
-                      res.render('index');
-                    } else {
-                    // Request was successfull
-                      console.log(response);
+      if (error) {
+        //request has failed
+        console.log(error);
+        res.render('index');
+      } else {
+      // Request was successfull
+        console.log(response);
 
-                      let sql = `SELECT tracker_ip FROM user, tracker WHERE user_id = tracker_user and user_name = '${username}'`;
-                      let query = db.query(sql, (err, result) => {
-                        if(err) throw err;
-                        console.log(result);
-
-                      if(result == req.connection.remoteAddress) {
-                        
-                        detect = require('./utils/browserDetect.js')
-                        validateConnecting("resilience34@outlook.fr")
-                        res.render('waitingPage', { id : response.id })
-
-                        console.log('ip '+ req.connection.remoteAddress)
-                        console.log('port '+ req.connection.remotePort)
-                        console.log('user is connect')  
-
-                      } else {
-                        validateConnecting("resilience34@outlook.fr")
-                        ipIssue("resilience34@outlook.fr")
-                        res.render('waitingPage', { id : response.id })
-                      }
-                    })
-
-                  }
-            })         
-  } else {
-    res.render('index')
+         let sql = `SELECT tracker_ip FROM user, tracker WHERE user_id = tracker_user and user_name = '${username}'`;
+         let query = db.query(sql, (err, result) => {
+           if(err) {
+             console.log(err);
+           } 
+           console.log(result);
+           if(result == request.connection.remoteAddress) {
+             detect = require('./utils/browserDetect.js')
+             validateConnecting("resilience34@outlook.fr")
+             res.render('waitingPage', { id : response.id })
+             console.log('ip '+ request.connection.remoteAddress)
+             console.log('port '+ request.connection.remotePort)
+             console.log('user is connect')  
+           } else {
+             validateConnecting("resilience34@outlook.fr")
+             ipIssue("resilience34@outlook.fr")
+             res.render('waitingPage', { id : response.id })
+           }
+        })
+      }
+    });
   }
 }
-
 
 
 app.get('/', function (req, res) {
@@ -116,12 +91,29 @@ app.post('/waitingPage',function (req, res) {
     if(connectUser(username, password) == false) { 
       res.render('error')
     } else {
+      
+      function authenticateDN(username, password){
+        var client = ldap.createClient({
+          url: 'ldap://192.168.1.18:5389'
+        })
+        client.bind(username,password,function(err){
+          if(err){
+            console.log("Error in new connection "+ err.code +err);
+            var authenticateldap = true 
+            connexion(authenticateldap, username,request)
+          } else {
+            console.log("success")
+            var authenticateldap = true
+            connexion(authenticateldap,username,res)
+          }
+        })
+      }
+
       authenticateDN("uid="+username+",ou=ourldap","")
+
     }   
   }
 })
-
-
 
 
 app.post('/connexion', function(req, res){
